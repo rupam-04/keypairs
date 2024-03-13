30b4f1d81bca66377ce7a4413b8025b3f08af66a
rupam-04 <rpmdey2004@gmail.com>
2024-03-14 01:37:09 +0530

    final

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ use lambdaworks_math::{cyclic_group::IsGroup, elliptic_curve::traits::IsElliptic
 type FEE = FieldElement<BLS12381PrimeField>;
 
 fn main() {
-    let generatoor = bls12_381::curve::BLS12381Curve::generator();
+    let gen = bls12_381::curve::BLS12381Curve::generator();
     let sk = 7809643498195481456u64;
-    let pk = generatoor.operate_with_self(sk);
-
-    println!("Computed public key: {:?}", pk);
+    let pk = gen.operate_with_self(sk);
 }
 
 #[cfg(test)]
@@ -20,13 +18,13 @@ mod tests {
     #[test]
     fn test_public_key_derivation() {
         let g = bls12_381::curve::BLS12381Curve::generator();
-        let sk = 7809643498195481456u64;
-        let pk = g.operate_with_self(sk);
+        let secret_key = 7809643498195481456u64;
+        let public_key = g.operate_with_self(secret_key);
 
         let res_x = FEE::new_base("67F9FFC5EAF6C19292112EADF50C11E7460E7568493141676F6BA1374BADD9F6AB1F2F5E155B0E3D2F4C1DE79554F80");
         let res_y = FEE::new_base("18509D22F2107B667A8F75DE737A4FB967F6C3E745A7C2361868515402318F006BD360B8A8763D7844381C6E510799CC");
         let expected_pk = bls12_381::curve::BLS12381Curve::create_point_from_affine(res_x, res_y).unwrap();
 
-        assert_eq!(expected_pk, pk, "this ain't it man");
+        assert_eq!(expected_pk, public_key, "this ain't it man");
     }
 }
